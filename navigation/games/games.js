d6aebf4e9d9df2acbc1e0c11c9c165604d32e7c6
FutureElliotto <156960234+FutureElliotto@users.noreply.github.com>
2025-09-05 21:50:49 -0400

    1.4 games 7.7

--- navigation/games/games.js
+++ navigation/games/games.js
@@ -6,7 +6,7 @@ let favorites = JSON.parse(localStorage.getItem("favorites")) || [];
 
 async function fetchGames() {
   try {
-    const response = await fetch("https://cdn.jsdelivr.net/gh/FutureElliotto/Arcade-4@1b6574b/navigation/games/games.json");
+    const response = await fetch("https://cdn.jsdelivr.net/gh/FutureElliotto/Arcade-4@59407d6/navigation/games/games.json");
     gamesData = await response.json();
     filteredGames = [...gamesData];
     renderPage();
