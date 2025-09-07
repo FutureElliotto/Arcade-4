2f73a5112313581e0fbe8b40435e01ec81920cb4
FutureElliotto <156960234+FutureElliotto@users.noreply.github.com>
2025-09-07 10:51:41 -0400

    Update home.js

--- navigation/home.js
+++ navigation/home.js
@@ -5,7 +5,7 @@
         "Whatever",
         "Etc.",
         "egassem sdrawkcab.",
-        "retsis ym tib ecno esoom A .esoom citsejam eht gnidulcnI ,slamina yrruf gnitseretni inam dnA ,metsys enohpelet lufrednow ehT ,sekal ilevol eht eeS ?rey siht nedeewS ni yadiloh a iert ton iW",
+        "Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.",
         "LOL",
         "You got games on your phone?",
         "Teeth are just tiny hardened bats sleeping upside down.",
